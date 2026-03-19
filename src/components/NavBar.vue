2b3b2c5d5a786edb7e04869f9ecf97abe05fde65
wse114 <2403039214@qq.com>
2026-03-19 23:17:39 +0800

    Initial commit:Vue3-Personal-Blog初始化

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<template>
+  <div>
+    <h1>NavBar</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomePart.vue')
+    },
+    {
+      path: '/tech-stack',
+      name: 'techStack',
+      component: () => import('../views/TechStack.vue')
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: () => import('../views/ExperiencePart.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutPart.vue')
+    },
+    {
+      path: '/sports',
+      name: 'sports',
+      component: () => import('../views/SportsPart.vue')
+    },
+    {
+      path: '/goals',
+      name: 'goals',
+      component: () => import('../views/GoalsPart.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginPart.vue')
+    }
+  ]
+})
+
+export default router
